aa19655020fc70f47cb2d9b678a3f5884702ca88
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-03-23 22:48:32 +0600

    bg added

--- App.js
+++ App.js
@@ -1,37 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import {
-  useFonts,
-  Inter_400Regular,
-  Inter_700Bold,
-} from "@expo-google-fonts/inter";
+  ActivityIndicator,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
-  let { fontRegular } = useFonts({
-    "Inter-Regular": Inter_400Regular,
-    "Inter-Bold": Inter_700Bold,
-  });
-
-  if (!fontsLoaded) {
-    return <ActivityIndicator />;
-  }
-
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "Inter-Regular", fontSize: 28 }}>
-        Inter-Regular
-      </Text>
-      <Text style={{ fontFamily: "Inter-Bold", fontSize: 28 }}>Inter-Bold</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require("./assets/bg.jpg")} style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          marginTop: 32,
+          paddingHorizontal: 26,
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <View>
+            <Text style={{ fontSize: 20, color: "#fff" }}>
+              Sunshine is delicious, rain is refreshing, wind braces us up, snow
+              is exhilarating; there is really no such thing as bad weather,
+              only different kinds of good weather.
+            </Text>
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
